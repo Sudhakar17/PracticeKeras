928e0c3624fd632fd83aff2851b10ab19a7d6d87
Sudhakar17 <sudhakarece17@gmail.com>
2020-04-28 10:20:04 +0200

    Added new CNN model

---
+++ CNN.py
@@ -0,0 +1,41 @@
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Flatten
+from keras.layers.normalization import BatchNormalization
+# from keras.regularizers import l2
+from keras.layers.convolutional import Conv2D
+from keras.layers.pooling import MaxPooling2D
+NUM_CLASSES = 10
+IMG_SIZE = 28
+
+
+def cnn_model():
+    model = Sequential()
+
+    model.add(Conv2D(32, (3, 3), padding='same',
+                     input_shape=(IMG_SIZE, IMG_SIZE,1),
+                     activation='relu'))
+    # model.add(BatchNormalization())
+    model.add(Conv2D(32, (3, 3), activation='relu'))
+    # model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+
+    model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
+    # model.add(BatchNormalization())
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    # model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+
+    model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
+    # model.add(BatchNormalization())
+    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+
+    model.add(Flatten())
+    model.add(Dense(512, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(NUM_CLASSES, activation='softmax'))
+
+    return model
